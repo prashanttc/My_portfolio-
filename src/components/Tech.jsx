428d77df84562d2d00ece85485c565fdcdbca764
prashant <prashantchouhan711@gmail.com>
2024-10-24 01:32:56 +0530

    dot-6

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import {BallCanvas} from "./canvas";
+import React, { useState, useEffect } from 'react';
+import { BallCanvas } from "./canvas";
 import { SectionWrapper } from '../hoc';
 import { technologies } from '../constants';
 
 const Tech = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  const [displayedTechnologies, setDisplayedTechnologies] = useState(technologies);
+
+  useEffect(() => {
+    
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isMobile) {
+      setDisplayedTechnologies(technologies.slice(0, 6));
+    } else {
+      setDisplayedTechnologies(technologies);
+    }
+  }, [isMobile]);
+
   return (
-    <div className='flex flex-row md:mx-32  flex-wrap justify-center gap-10'>
-      {technologies.map((technologies)=>(
-        <div className='md:w-28 md:h-28 w-16 h-16' key={technologies.name}>
-<BallCanvas icon={technologies.icon}/>
+    <div className='flex flex-row md:mx-32 flex-wrap justify-center gap-10'>
+      {displayedTechnologies.map((tech) => (
+        <div className='md:w-28 md:h-28 w-16 h-16' key={tech.name}>
+          <BallCanvas icon={tech.icon} />
         </div>
       ))}
     </div>
-  )
+  );
 }
 
-export default SectionWrapper(Tech,"tech")
+export default SectionWrapper(Tech, "tech");
