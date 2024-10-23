db3ef0a8b75d0e76657a4c03941a15d65b376c32
prashant <prashantchouhan711@gmail.com>
2024-10-23 19:13:12 +0530

    changes

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -2,6 +2,7 @@ import React,{Suspense} from 'react';
 import { Canvas } from '@react-three/fiber';
 import { Decal,Float,OrbitControls,Preload,useTexture } from '@react-three/drei';
 import Loader from "../Loader"
+import { MeshBasicMaterial } from 'three';
 
 const   Ball = (props) => {
   const[decal] = useTexture([props.imgUrl])
@@ -11,7 +12,7 @@ const   Ball = (props) => {
       <directionalLight position={[0,0,0.05]}/>
       <mesh castShadow receiveShadow scale={2.75}>
         <icosahedronGeometry args={[1,1]}/>
-        <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
+        <MeshBasicMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
         <Decal position={[0,0,1]} map={decal} rotation={[2 *Math.PI, 0, 6.25]}/>
       </mesh>
     </Float>
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,82 +1,82 @@
-import React, { Suspense, useState , useEffect } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, SpotLight, useGLTF } from '@react-three/drei';
+  import React, { Suspense, useState , useEffect } from 'react';
+  import { Canvas } from '@react-three/fiber';
+  import { OrbitControls, Preload, SpotLight, useGLTF } from '@react-three/drei';
 
-import CanvasLoader from "../Loader";
+  import CanvasLoader from "../Loader";
 
-const Computers = ({isMobile}) => {
-  const computer = useGLTF('/desktop_pc/scene.gltf');
-  
-  return (
-    <mesh receiveShadow castShadow>
-      <hemisphereLight 
-        intensity={0.5} 
-        groundColor="gray" 
-        skyColor="#ffffff"
-      />
-      <ambientLight intensity={0.5} color="#ffffff" />
-      <pointLight 
-        position={[10, 10, 10]} 
-        intensity={2} 
-        color="#ffffff" 
-        castShadow
-      />
-      <SpotLight 
-        position={[0, 20, 10]}  
-        angle={0.3}
-        penumbra={0.5}
-        intensity={3.5}
-        castShadow
-        shadow-mapSize={2048} 
-        color="#f8e6a0"
-      />
-      <directionalLight 
-        position={[5, 10, 5]} 
-        intensity={1.5} 
-        shadow-mapSize={2048}
-        castShadow 
-      />
-      <primitive 
-        object={computer.scene} 
-        scale={isMobile?0.50:0.75}
-        position={isMobile?[0, -2.8, -1.8]:[0,-3.8,-1.0]} 
-        rotation={[-0.01, -0.2, -0.1]}
-        receiveShadow
-        castShadow
-      />
-    </mesh>
-  );
-};
-
-const ComputerCanvas = () => {
-  const [isMobile , setIsMobile]=  useState(false)
-
-  useEffect(() => {
-    const mediaQuery = window.matchMedia('(max-width:500px)');
-    setIsMobile(mediaQuery.matches)
-    const handleMediaQueryChange = (event)=>{
-setIsMobile(event.matches)
-    } 
-    mediaQuery.addEventListener("change",
-      handleMediaQueryChange
-    )
-    return ()=>{
-    mediaQuery.removeEventListener("change", handleMediaQueryChange)
-  }
-  }, [])
-  return (
-    <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
-      <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls 
-          enableZoom={false} 
-          maxPolarAngle={Math.PI / 2}
-          minPolarAngle={Math.PI / 2} 
+  const Computers = ({isMobile}) => {
+    const computer = useGLTF('/desktop_pc/scene.gltf');
+    
+    return (
+      <mesh receiveShadow castShadow>
+        <hemisphereLight 
+          intensity={0.5} 
+          groundColor="gray" 
+          skyColor="#ffffff"
         />
-        <Computers isMobile={isMobile} />
-      </Suspense>
-      <Preload all />
-    </Canvas>
-  );
-};
+        <ambientLight intensity={0.5} color="#ffffff" />
+        <pointLight 
+          position={[10, 10, 10]} 
+          intensity={isMobile?1:2} 
+          color="#ffffff" 
+          castShadow
+        />
+        <SpotLight 
+          position={[0, 20, 10]}  
+          angle={0.3}
+          penumbra={0.5}
+          intensity={isMobile?1:3.5}
+          castShadow
+          shadow-mapSize={2048} 
+          color="#f8e6a0"
+        />
+        <directionalLight 
+          position={[5, 10, 5]} 
+          intensity={isMobile?0.5 :1.5} 
+          shadow-mapSize={isMobile?1024:2048}
+          castShadow 
+        />
+        <primitive 
+          object={computer.scene} 
+          scale={isMobile?0.50:0.75}
+          position={isMobile?[0, -2.8, -1.8]:[0,-3.8,-1.0]} 
+          rotation={[-0.01, -0.2, -0.1]}
+          receiveShadow
+          castShadow
+        />
+      </mesh>
+    );
+  };
 
-export default ComputerCanvas;
+  const ComputerCanvas = () => {
+    const [isMobile , setIsMobile]=  useState(false)
+
+    useEffect(() => {
+      const mediaQuery = window.matchMedia('(max-width:500px)');
+      setIsMobile(mediaQuery.matches)
+      const handleMediaQueryChange = (event)=>{
+  setIsMobile(event.matches)
+      } 
+      mediaQuery.addEventListener("change",
+        handleMediaQueryChange
+      )
+      return ()=>{
+      mediaQuery.removeEventListener("change", handleMediaQueryChange)
+    }
+    }, [])
+    return (
+      <Canvas frameloop="demand" shadows camera={{ position: [20, 3, 5], fov: 25 }} gl={{ preserveDrawingBuffer: true }}>
+        <Suspense fallback={<CanvasLoader />}>
+          <OrbitControls 
+            enableZoom={false} 
+            maxPolarAngle={Math.PI / 2}
+            minPolarAngle={Math.PI / 2} 
+          />
+          <Computers isMobile={isMobile} />
+        </Suspense>
+        <Preload all />
+      </Canvas>
+    );
+  };
+
+  export default ComputerCanvas;
