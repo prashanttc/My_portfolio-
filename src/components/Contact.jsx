a76251ce918fad450cfd81fb2ad3b75e44860d75
prashant <prashantchouhan711@gmail.com>
2024-10-23 22:38:44 +0530

    cc on contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import { styles } from "../styles";
 import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
+import ComputerCanvas from "./canvas/Computers";
 
 const Contact = () => {
   const formRef = useRef();
@@ -127,7 +128,7 @@ const Contact = () => {
         variants={slideIn("right", "tween", 0.2, 1)}
         className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
       >
-        <EarthCanvas />
+        <ComputerCanvas />
       </motion.div>
     </div>
   );
